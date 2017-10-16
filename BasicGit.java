50f11648cd8db67423c0a293951dd208e166f6a4
Nick Taglianetti <nick_taglianetti@yahoo.com>
2017-10-16 13:30:59 -0400

    final

---
+++ BasicGit.java
@@ -0,0 +1,11 @@
+// Nick Taglianetti
+// CS1530 Exercise 2: Basic Git
+
+public class BasicGit {
+	public static void main(String[] args) {
+		for (int i=1; i<11; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
